fdb231beae081f18cb1a36d8df32aa714e1398ae
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-07-05 15:20:16 +0200

    oauth authentication commit

--- application/controllers/oauth2.php
+++ application/controllers/oauth2.php
@@ -12,12 +12,49 @@ class oauth2 extends CI_Controller {
     	 * - token for this user?
     	 * - ...
     	 */
+        $this->load->database();
         
-        // identify client
+        // required
         $client_id = $this->input->get('client_id');
+        $client_secret = $this->input->get('client_secret');
         $response_type = $this->input->get('response_type');
+        // optional
         $callback = $this->input->get('redirect_uri');
         $state = $this->input->get('state');
+        
+        // prepare
+        $data = new stdClass();
+        
+        // invalid_request
+        if( !( $client_id && $client_secret && $response_type ) ){
+            $data->error = "1" ;
+            $data->error_msg = 'invalid_request';
+            $this->load->view('authenticate', $data);
+            return ;
+        }
+        
+        // unsupported_response_type
+        if( $response_type != 'code' ){
+            $data->error = "4" ;
+            $data->error_msg = "unsupported_response_type" ;
+            $this->load->view('authenticate', $data);
+            return ;
+        }
+        
+        // unauthorized_client
+        $where = array('client_id' => $client_id, 'client_secret' => $client_secret );
+        $query = $this->db->get_where('clients', $where );
+        
+        if( $query->num_rows() != 1 ){
+            $data->error = "2" ;
+            $data->error_msg = "unauthorized_client" ;
+            $this->load->view('authenticate', $data);
+            return ;
+        }
+        $row = $query->row();
+        $name = $row->name ;
+        // optional callback
+        $callback = $callback? $callback : $row->redirect_uri ;
         
         // allow button clicked
         if ($this->input->post('allows')) {
@@ -25,6 +62,13 @@ class oauth2 extends CI_Controller {
             $code = md5(time() . uniqid());
             
             // save code to database
+            $row = array(
+                'client_id' => $client_id,
+                'user_id' => 'temp', // CHANGE TH!S
+                'code' => $code,
+                'expires' => time() + 60 * 10 // ten minutes
+            );
+            $this->db->insert('auth_codes', $row);
             
             // generate callback url
             $callback = $callback . '?' . http_build_query(array('code' => $code, 'state' => $state));
@@ -33,6 +77,7 @@ class oauth2 extends CI_Controller {
             redirect($callback);
         } else {
         	// show access screen
+            $data->client = $name ;
             $this->load->view('authenticate', $data);
         }
     }
@@ -44,6 +89,7 @@ class oauth2 extends CI_Controller {
     	 */
     	
     	$grant_type = $this->input->get('grant_type');
+        $code = $this->input->get('code');
     }
 
 }
--- application/views/authenticate.php
+++ application/views/authenticate.php
@@ -1,11 +1,17 @@
 <?php include('header.tpl'); ?>
 
 <div class="container">
-
+    
 	<div class="hero-unit">
-		<h1><?php echo $client; ?> requests access</h1>
-		<p>Connecting this app will allow it to do certain things like create new check-ins on your behalf and access personal information such as your profile information, check-in history, friends list, tips and to-dos.</p>
-		<p><a href="<?php echo $callback; ?>" class="btn btn-primary btn-large">Allow</a></p>
+        <?php if( !$error ){ ?>
+		    <h1><?php echo $client; ?> requests access</h1>
+		    <p>Connecting this app will allow it to do certain things like create new check-ins on your behalf and access personal information such as your profile information, check-in history, friends list, tips and to-dos.</p>
+            <form>
+		        <p><button class="btn btn-primary btn-large" value="Allow" /></p>
+            </form>
+        <?php }else{ ?>
+            <label class="error-msg"><?php echo $error_msg; ?></label>
+        <?php } ?>
 	</div>
 
 </div>
