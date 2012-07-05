612fbe84880da48c9031bd12769fbdf5d47b09de
Koen De Groote <kdg.private@gmail.com>
2012-07-05 15:38:35 +0200

    Update with twitter_api and Oauth1_client. 100% copy from https://github.com/abraham/twitteroauth. Also, development config added for twitter key and secret

---
+++ application/config/development/twitter.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+    //Currently using the configuration of koen's test app.
+    $config['CONSUMER_KEY'] = 'ZKIzflTWce8uVIdthw';
+    $config['CONSUMER_SECRET'] = 'pVhfAojMdQqnujWGLJ9RwSyr8uJ6XCm3R5MvfzVmiP4';
+    $config['OAUTH_CALLBACK'] = 'http://kdgsolomidemtest.com/callback.php';
+
+?>
