5f5cc98ab48abe839c341e86de014e3ac6377446
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-07-05 13:36:04 +0200

    sql updated

--- doc/sql/auth-server.sql
+++ doc/sql/auth-server.sql
@@ -1,58 +1,70 @@
-CREATE TABLE `clients` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(64) NOT NULL DEFAULT '',
-  `client_id` VARCHAR(32) NOT NULL DEFAULT '',
-  `client_secret` VARCHAR(32) NOT NULL DEFAULT '',
-  `redirect_uri` VARCHAR(250) NOT NULL DEFAULT '',
-  `auto_approve` TINYINT(1) NOT NULL DEFAULT '0',
-  `autonomous` TINYINT(1) NOT NULL DEFAULT '0',
-  `status` ENUM('development','pending','approved','rejected') NOT NULL DEFAULT 'development',
-  `suspended` TINYINT(1) NOT NULL DEFAULT '0',
-  `notes` TINYTEXT,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `client_id` (`client_id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `scopes` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `scope` VARCHAR(64) NOT NULL DEFAULT '',
-  `name` VARCHAR(64) NOT NULL DEFAULT '',
-  `description` VARCHAR(100) DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `scope` (`scope`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `sessions` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `client_id` VARCHAR(32) NOT NULL DEFAULT '',
-  `redirect_uri` VARCHAR(250) NOT NULL DEFAULT '',
-  `type_id` VARCHAR(64) DEFAULT NULL,
-  `type` ENUM('user','auto') NOT NULL DEFAULT 'user',
-  `code` TEXT,
-  `access_token` VARCHAR(50) DEFAULT '',
-  `stage` ENUM('request','granted') NOT NULL DEFAULT 'request',
-  `first_requested` INT(10) UNSIGNED NOT NULL,
-  `last_updated` INT(10) UNSIGNED NOT NULL,
-  `limited_access` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Used for user agent flows',
-  PRIMARY KEY (`id`),
+CREATE TABLE IF NOT EXISTS `auth_codes` (
+  `client_id` varchar(32) NOT NULL,
+  `user_id` varchar(32) NOT NULL,
+  `code` varchar(40) NOT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `unique` (`client_id`,`user_id`),
+  KEY `user_id` (`user_id`),
   KEY `client_id` (`client_id`)
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `session_scopes` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `session_id` INT(11) UNSIGNED NOT NULL,
-  `access_token` VARCHAR(50) NOT NULL DEFAULT '',
-  `scope` VARCHAR(64) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `session_id` (`session_id`),
-  KEY `scope` (`scope`),
-  KEY `access_token` (`access_token`),
-  CONSTRAINT `session_scopes_ibfk_1` FOREIGN KEY (`scope`) REFERENCES `scopes` (`scope`),
-  CONSTRAINT `session_scopes_ibfk_2` FOREIGN KEY (`session_id`) REFERENCES `sessions` (`id`) ON DELETE CASCADE
-) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE  `sessions` ADD FOREIGN KEY (`client_id`) REFERENCES  `clients` (
-`client_id`
-) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS `auth_tokens` (
+  `client_id` varchar(32) NOT NULL,
+  `user_id` varchar(40) NOT NULL,
+  `auth_token` varchar(40) NOT NULL,
+  `expires` int(11) NOT NULL,
+  UNIQUE KEY `unique` (`client_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `client_id` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `client_secret` varchar(32) NOT NULL,
+  `redirect_uri` varchar(250) NOT NULL,
+  `status` enum('rejected','approved') NOT NULL DEFAULT 'approved',
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `services` (
+  `service_id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(140) NOT NULL,
+  PRIMARY KEY (`service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` varchar(32) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `user_tokens` (
+  `user_id` varchar(32) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `access_token` varchar(40) NOT NULL,
+  `refresh_token` varchar(40) DEFAULT NULL,
+  UNIQUE KEY `unique` (`service_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  KEY `service_id` (`service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `auth_codes`
+  ADD CONSTRAINT `auth_codes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `auth_codes_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `auth_tokens`
+  ADD CONSTRAINT `auth_tokens_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `auth_tokens_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE;
+
+
+ALTER TABLE `user_tokens`
+  ADD CONSTRAINT `user_tokens_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `services` (`service_id`) ON DELETE CASCADE;
