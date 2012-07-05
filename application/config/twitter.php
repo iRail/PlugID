fcdca09ade0f296d1c6a3ab3322266847154e5c5
Koen De Groote <kdg.private@gmail.com>
2012-07-05 15:51:05 +0200

    Update with twitter_api and Oauth1_client. 100% copy from https://github.com/abraham/twitteroauth. Also, empty config added.

---
+++ application/config/twitter.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+    $config['CONSUMER_KEY'] = '';
+    $config['CONSUMER_SECRET'] = '';
+    $config['OAUTH_CALLBACK'] = '';
+
+?>
