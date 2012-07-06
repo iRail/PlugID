b9c7b570cf224ad986d633d066b23df387cbc211
Koen De Groote <kdg.private@gmail.com>
2012-07-06 14:30:03 +0200

    Adding Vikingspots lib and API. Basic copy from FS lib+api with all configs and urls replaced.

---
+++ application/config/vikingspots.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+    $config['client_id'] = '';
+    $config['client_secret'] = '';
+    $config['push_secret'] = '';
+    $config['callback_url'] = '';
+    
+?>
