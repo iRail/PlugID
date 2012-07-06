42f45b98aefac7eb85a55808fca771f6454bfe76
Jens Segers <segers.jens@gmail.com>
2012-07-06 11:25:39 +0200

    OAuth 2 client

--- application/libraries/OAuth2_client.php
+++ application/libraries/OAuth2_client.php
@@ -1,25 +1,73 @@
 <?php
 
-abstract class OAuth2_client extends client {
+class OAuth2_client extends client {
     
-	function get_access_token();
+    public $url_authorize = '';
+    public $url_access_token = '';
+    public $callback;
+    public $client_id = '';
+    public $client_secret = '';
     
-    function create_authorize_url();
-    
-    function api();
-
-    function set_token( $toke );
-    
-    function api( $uri, $data = array(), $method = 'GET' );
-
-    function get_userid();
-    
-    protected function _get(){
-        $c = curl_init();
-        curl_setopt($c, CURLOPT_URL, $url);
-        curl_setopt($c, CURLOPT_HTTPGET, true );
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE); 
+    /**
+     * Redirect to authorize url
+     * @param array $options
+     */
+    public function authorize($options = array()) {
+        // build params
+        $params = array();
+        $params['client_id'] = $this->client_id;
+        $params['redirect_uri'] = isset($options['redirect_uri']) ? $options['redirect_uri'] : $this->redirect_uri;
+        $params['state'] = md5(uniqid(rand(), TRUE));
+        $params['response_type'] = 'code';
         
-        return curl_exec($c);
+        if ($options['scope']) {
+            $params['scope'] = $options['scope'];
+        }
+        
+        // redirect
+        redirect($this->url_authorize . '?' . http_build_query($params));
     }
+    
+    /**
+     * Request access token from code
+     * @param string $code
+     * @param array $options
+     */
+    public function access_token($code, $options = array()) {
+        $params = array();
+        $params['code'] = $code;
+        $params['redirect_uri'] = isset($options['redirect_uri']) ? $options['redirect_uri'] : $this->redirect_uri;
+        $params['grant_type'] = 'authorization_code';
+        $params['client_id'] = $this->client_id;
+        $params['client_secret'] = $this->client_secret;
+        
+        $ci = &get_instance();
+        $ci->load->library('curl');
+        
+        $data = $ci->curl->get($path, $params);
+        $json = json_decode($data);
+        
+        // response
+        $token = $json->access_token;
+        $expires = $json->expires_in ? $json->expires_in : FALSE;
+        $refresh_token = $json->refresh_token ? $json->refresh_token : FALSE;
+        $scope = $json->scope ? $json->scope : FALSE;
+    }
+    
+    /**
+     * Make API calls
+     * @param string $path
+     * @param string $method
+     * @param array $params
+     */
+    public function api($path, $method = 'GET', $params = array()) {
+        $ci = &get_instance();
+        $ci->load->library('curl');
+        
+        $method = strtolower($method);
+        
+        $data = $ci->curl->$method($path, $params);
+        return json_decode($data);
+    }
+
 }
---
+++ application/libraries/cURL.php
@@ -0,0 +1,59 @@
+<?php
+
+class cURL {
+    
+    public $error_code;
+    public $error;
+    
+    public function get($url, $params = array()) {
+        // build query string
+        if (is_array($params)) {
+            $params = http_build_query($params, NULL, '&');
+        }
+        
+        // add parameters to url
+        $url = strstr($url, '?') ? $url . '&' . $params : $url . '?' . $params;
+        
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HTTPGET, TRUE);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        
+        return $this->execute($curl);
+    }
+    
+    public function post($url, $params = array()) {
+        // build query string
+        if (is_array($params)) {
+            $params = http_build_query($params, NULL, '&');
+        }
+        
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        
+        return $this->execute($curl);
+    }
+    
+    private function execute($curl) {
+        // execute
+        $data = curl_exec($curl);
+        
+        // error
+        if ($data === FALSE) {
+            $errno = curl_errno($curl);
+            $error = curl_error($curl);
+            
+            curl_close($curl);
+            
+            $this->error_code = $errno;
+            $this->error = $error;
+            return FALSE;
+        }
+        
+        curl_close($curl);
+        return $data;
+    }
+}
