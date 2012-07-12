577113b111e87f3f70b5f26f7590d5ecc6917bbe
Jens Segers <segers.jens@gmail.com>
2012-07-12 09:18:22 +0200

    Bug fixed, I hope

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -12,77 +12,77 @@ class api extends CI_Controller {
         $this->load->model('access_token_model');
         $this->load->model('user_model');
         
-        $service_name_index = 2 ;
+        $service_name_index = 2;
         
         // at least 3
         $num_segments = $this->uri->total_segments();
-        if( $num_segments < 3 ){
-            $this->return_error( array('error' => 'Not enough parameters. We need "api/SERVICE_NAME/ENDPOINT_URI" .' ));
+        if ($num_segments < 3) {
+            $this->return_error(array('error' => 'Not enough parameters. We need "api/SERVICE_NAME/ENDPOINT_URI" .'));
         }
         
         // build endpoint_uri
         $segments = $this->uri->segment_array();
         $endpoint_uri = '';
-        for( $i = $service_name_index + 1 ; $i <= $num_segments ; $i += 1 ){
-            $endpoint_uri .= $segments[$i] . '/' ;
+        for ($i = $service_name_index + 1; $i <= $num_segments; $i += 1) {
+            $endpoint_uri .= $segments[$i] . '/';
         }
-        $endpoint_uri = rtrim($endpoint_uri,'/');
+        $endpoint_uri = rtrim($endpoint_uri, '/');
         
         // get service_name
-        $service_name = $segments[$service_name_index] ;
+        $service_name = $segments[$service_name_index];
         
         // get authentication token
-        $error = FALSE ;
-        $tokens = NULL ;
+        $error = FALSE;
+        $tokens = NULL;
         $auth_header = $this->input->get_request_header('Authorization');
-        if( !preg_match('/^Bearer\ (.*)$/', $auth_header, $matches)){
+        if (!preg_match('/^Bearer\ (.*)$/', $auth_header, $matches)) {
             // no authentication was given
-            $error = array('error'=>'no authentication was given');
-        }else{
+            $error = array('error' => 'no authentication was given');
+        } else {
             // validate token
-            $row = $this->access_token_model->is_valid( $matches[1] );
-            if (!isset($row->user_id)){
+            $row = $this->access_token_model->is_valid($matches[1]);
+            if (!isset($row->user_id)) {
                 // authentication was tried and failed
-                $error = array('error'=>'invalid access_token');
-            }else{
+                $error = array('error' => 'invalid access_token');
+            } else {
                 $tokens = $this->user_model->get_tokens($row->user_id, $service_name);
             }
         }
         
-        if( $error ){
+        if ($error) {
             $this->return_error($error);
         }
         
-        $get_params  = $this->input->get();
+        $get_params = $this->input->get();
         $post_params = $this->input->post();
         
-        $method = 'get' ; // default
+        $method = 'get'; // default
         $params = array();
-        if( $get_params !== FALSE ){
+        if ($get_params !== FALSE) {
             $method = 'get';
             $params = $get_params;
-        }else if( $post_params !== FALSE ){
+        } else if ($post_params !== FALSE) {
             $method = 'post';
             $params = $post_params;
-        }else{
-            if( $postbody = file_get_contents('php://input')){
+        } else {
+            if ($postbody = file_get_contents('php://input')) {
                 $this->return_error(array('error' => 'plain postbody not yet supported'));
             }
         }
         
         // Y U NO LOAD?
         $this->load->driver('service', array('adapter' => $service_name));
-        if( !isset( $this->service->$service_name )){
-            $this->return_error( array( 'error' => $service_name . ' does not exist' ));
+        if (!$this->service->is_valid($service_name)) {
+            $this->return_error(array('error' => $service_name . ' does not exist'));
         }
         
-        $this->service->$service_name->set_authentication( $tokens );
-        echo $this->service->$service_name->api($endpoint_uri, $params, $method );
+        $this->service->{$service_name}->set_authentication($tokens);
+        echo $this->service->{$service_name}->api($endpoint_uri, $params, $method);
     }
     
-    private function return_error( $error ){
+    private function return_error($error) {
         $this->output->set_content_type('application/json');
-        $this->output->set_output(json_encode( $error ));
-        exit;
+        $this->output->set_output(json_encode($error));
+        exit();
     }
 }
--- application/libraries/Service/Service.php
+++ application/libraries/Service/Service.php
@@ -11,8 +11,8 @@ if (!defined('BASEPATH'))
 
 class Service extends CI_Driver_Library {
     
-    protected $adapter = NULL ;
-    protected $valid_drivers = array('Service_foursquare','Service_facebook','Service_google','Service_viking');
+    protected $adapter = NULL;
+    protected $valid_drivers = array('Service_foursquare', 'Service_facebook', 'Service_google', 'Service_viking');
     
     public function __construct($config = array()) {
         if (isset($config['adapter']) && in_array('service_' . $config['adapter'], array_map('strtolower', $this->valid_drivers))) {
@@ -23,31 +23,31 @@ class Service extends CI_Driver_Library {
     function __call($method, $args = array()) {
         return call_user_func_array(array($this->{$this->adapter}, $method), $args);
     }
-    /*
-    function __isset( $driver_adapter ){
-        return $this->adapter == $driver_adapter ;
-    }*/
+    
+    function is_valid($driver) {
+        return in_array($driver, $this->valid_drives);
+    }
 }
 
 /*
  * Class to abstract functions from drivers
  */
 abstract class Abstract_service extends CI_Driver {
-    protected $ci ;
-    protected $settings ;
+    protected $ci;
+    protected $settings;
     private $hash_algo = 'md5';
     
     /**
      * Constructor
      */
-    function __construct(){
+    function __construct() {
         $this->ci = &get_instance();
     }
     
     /**
      * Get ext_user_id and authentication_tokens from specific service
      */
-    abstract function callback( $callback_data );
+    abstract function callback($callback_data);
     
     /**
      * Gets all params ready and calls build_and_redirect(
@@ -57,43 +57,42 @@ abstract class Abstract_service extends CI_Driver {
     /**
      * Get all necessary tokens to sign requests
      */
-    abstract function set_authentication( $tokens );
+    abstract function set_authentication($tokens);
     
     /**
      * proxy calls
      */
-    public function api( $endpoint, $params = array(), $method = 'get' );
+    public function api($endpoint, $params = array(), $method = 'get');
     
     /**
      * Makes config loading easier
      */
-    protected function load_config( $name, $conf_dir = NULL ){
-        $file  = is_null( $conf_dir ) ? '' : rtrim($conf_dir,'/') . '/';
-        $file .= $name ;
-        $this->ci->config->load( $file , TRUE);
-        $this->settings = $this->ci->config->item( $file );
+    protected function load_config($name, $conf_dir = NULL) {
+        $file = is_null($conf_dir) ? '' : rtrim($conf_dir, '/') . '/';
+        $file .= $name;
+        $this->ci->config->load($file, TRUE);
+        $this->settings = $this->ci->config->item($file);
     }
     
     /**
      * Simply rounds of this authorization request
      */
-    protected function build_and_redirect( $params ){
-        redirect( $this->settings['url_authorize'] . '?' . http_build_query($params) );
+    protected function build_and_redirect($params) {
+        redirect($this->settings['url_authorize'] . '?' . http_build_query($params));
     }
     
     /**
      * Generic function to provide a state for securing authorize request
      * Saves the state in Session
      */
-    protected function get_state(){
-        return $this->ci->session->state = hash($this->hash_algo, time() . uniqid()) ;
+    protected function get_state() {
+        return $this->ci->session->state = hash($this->hash_algo, time() . uniqid());
     }
 }
 
-
-abstract class Abstract_oauth2_service extends Abstract_service{
+abstract class Abstract_oauth2_service extends Abstract_service {
     
-    function __construct(){
+    function __construct() {
         parent::__construct();
     }
     
@@ -101,10 +100,11 @@ abstract class Abstract_oauth2_service extends Abstract_service{
     //abstract function authorize();
     //abstract function set_authentication( $tokens );
     
-    protected function setup_oauth_client_lib(){
-        $this->ci->oauth2->callback_url     = $this->settings['callback_url'];
-        $this->ci->oauth2->url_authorize    = $this->settings['url_authorize'];
+
+    protected function setup_oauth_client_lib() {
+        $this->ci->oauth2->callback_url = $this->settings['callback_url'];
+        $this->ci->oauth2->url_authorize = $this->settings['url_authorize'];
         $this->ci->oauth2->url_access_token = $this->settings['url_access_token'];
-        $this->ci->oauth2->url_api_base     = $this->settings['url_api_base'];
+        $this->ci->oauth2->url_api_base = $this->settings['url_api_base'];
     }
 }
