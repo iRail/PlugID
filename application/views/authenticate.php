d3031b556161262533ad8b1133a18d6ee9b105dd
Koen De Groote <kdg.private@gmail.com>
2012-07-12 17:36:24 +0200

    Can now view authenticate page after one link has already been made. Services already linked to the user are now spans. Don't know bootstrap well enough to know how to make button look disabled

--- application/views/authenticate.php
+++ application/views/authenticate.php
@@ -4,10 +4,26 @@
     <div class="hero-unit">
         <h1>Login</h1>
         <p>
-            <a href="<?php echo site_url('connect/facebook'); ?>" class="btn btn-large" >Facebook</a>
-            <a href="<?php echo site_url('connect/twitter'); ?>" class="btn btn-large" >Twitter</a>
-            <a href="<?php echo site_url('connect/viking'); ?>" class="btn btn-large" >Vikingspots</a>
-            <a href="<?php echo site_url('connect/foursquare'); ?>" class="btn btn-large" >Foursquare</a>
+            <?php if (in_array('facebook', $services)) { ?>
+                <span class="btn btn-large" >Facebook</span>
+            <?php } else { ?>
+                <a href="<?php echo site_url('connect/facebook'); ?>" class="btn btn-large" >Facebook</a>
+            <?php } ?>
+            <?php if (in_array('twitter', $services)) { ?>
+                <span class="btn btn-large" >Twitter</span>
+            <?php } else { ?>
+                <a href="<?php echo site_url('connect/twitter'); ?>" class="btn btn-large" >Twitter</a>
+            <?php } ?>
+            <?php if (in_array('viking', $services)) { ?>
+                <span class="btn btn-large" >Vikingspots</span>
+            <?php } else { ?>
+                <a href="<?php echo site_url('connect/viking'); ?>" class="btn btn-large" >Vikingspots</a>
+            <?php } ?>
+            <?php if (in_array('foursquare', $services)) { ?>
+                <span class="btn btn-large" >Foursquare</span>
+            <?php } else { ?>
+                <a href="<?php echo site_url('connect/foursquare'); ?>" class="btn btn-large" >Foursquare</a>
+            <?php } ?>
         </p>
         </form>
     </div>
