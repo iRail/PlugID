1fe895fdf0f8e853059b760031b8789a7c9c6f49
Koen De Groote <kdg.private@gmail.com>
2012-07-12 16:13:08 +0200

    Revoke view and basic functionality implemented.

---
+++ application/controllers/revoke.php
@@ -0,0 +1,53 @@
+<?php
+
+    /**
+     * @copyright (C) 2012 by iRail vzw/asbl
+     * @license AGPLv3
+     * @author Jens Segers <jens at iRail.be>
+     * @author Hannes Van De Vreken <hannes at iRail.be>
+     * @author Koen De Groote <koen at iRail.be>
+     * 
+     * Register a new client for this application's Oauth2.0 server
+     */
+    if (!defined('BASEPATH'))
+        exit('No direct script access allowed');
+
+    class Revoke extends CI_Controller {
+
+        function index() {
+            // check if signed in. Can't revoke access if not logged in.
+            $user_id = $this->session->user;
+
+            // it's a no go
+            if ($user_id === FALSE) {
+                redirect('authenticate');
+            }
+
+            //Load user model
+            $this->load->model('user_model');
+
+            $clients = $this->user_model->get_clients($user_id);
+
+
+            $client_id = $this->input->post('revoke');
+            //0 if not clicked. Client_id if clicked
+            if ($client_id != 0) {
+                //Revoke access, should work once auth_* tables get content
+                $this->user_model->revoke($user_id, $client_id);
+                redirect('revoke');
+            } else {
+                // show access screen
+                $data = array();
+                $clientinfo = array();
+                foreach ($clients as $client) {
+                    $vars = get_object_vars($client);
+                    array_push($data, $vars);
+                }
+                $clientinfo['multi'] = $data;
+                $this->load->view('revoke', $clientinfo);
+            }
+        }
+
+    }
+
+?>
