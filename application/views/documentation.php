234d456761dfb5057f21a1c9f1bfd817ef39c483
Koen De Groote <kdg.private@gmail.com>
2012-07-13 10:07:23 +0200

    Default view file for documentation. Need to link local to remote branch

---
+++ application/views/documentation.php
@@ -0,0 +1,10 @@
+<?php include('header.tpl'); ?>
+
+<div class="container">    
+    <div class="hero-unit">
+        <h1>All your registered client applications</h1>
+        <p></p>
+    </div>
+</div>
+
+<?php include ('footer.tpl'); ?>
