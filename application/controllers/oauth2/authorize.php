b35c7f3f494fdd5c978a5c198a57fd76f3f1a7dc
Jens Segers <segers.jens@gmail.com>
2012-07-13 10:02:54 +0200

    Duplicate class fix

--- application/controllers/oauth2/authorize.php
+++ application/controllers/oauth2/authorize.php
@@ -9,15 +9,9 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Oauth2 extends CI_Controller {
-    
-    private $ci;
-    
+class Authorize extends CI_Controller {
+	
     function index() {
-        redirect('register');
-    }
-    
-    function authorize() {
         $this->ci = &get_instance();
         $this->ci->load->library('Session');
         $this->ci->load->model('user_model');
@@ -98,11 +92,11 @@ class Oauth2 extends CI_Controller {
             // Generate callback url
             $params = array();
             $params['code'] = $code;
-            if( $state !== FALSE ){
+            if ($state !== FALSE) {
                 $params['state'] = $state;
             }
             
-            $redirect_uri .= (strpos($redirect_uri, '?') ? '?' : '&') . http_build_query( $params );
+            $redirect_uri .= (strpos($redirect_uri, '?') ? '?' : '&') . http_build_query($params);
             
             // Redirect back to user website
             redirect($redirect_uri);
@@ -113,60 +107,4 @@ class Oauth2 extends CI_Controller {
             $this->load->view('authorize', $data);
         }
     }
-    
-    function access_token() {
-        $data = array();
-        
-        // Required
-        $grant_type = $this->input->post('grant_type');
-        $code = $this->input->post('code');
-        $redirect_uri = $this->input->post('redirect_uri');
-        $client_id = $this->input->post('client_id');
-        
-        // Client secret from basic auth header OR post param
-        $client_secret = $this->input->get_request_header('Authorization');
-        if ($client_secret !== FALSE && preg_match('/^Basic\ (\w{32})$/', $client_secret, $matches)) {
-            $client_secret = $matches[1];
-        } else {
-            $client_secret = $this->input->post('client_secret');
-        }
-        
-        $this->load->model('code_model');
-        $this->load->model('client_model');
-        
-        // Client_secret must be given either way
-        if (!$client_secret || !$grant_type || !$code || !$client_id || !$redirect_uri) {
-            $data['error'] = 'invalid_request';
-        
-        // Hard-coded: 'grant-type' must be 'authorization_code'
-        } else if ($grant_type != 'authorization_code') {
-            $data['error'] = 'unsupported_grant_type'; //'invalid_grant' ;
-        
-
-        // Authenticate client
-        } else if (!$this->client_model->validate_secret($client_id, $client_secret)) {
-            $data['error'] = 'invalid_client';
-        
-        // Validate code
-        } else if (!$this->client_model->validate_redirect_uri($client_id, $redirect_uri)) {
-            $data['error'] = '';
-        
-        // Validate code
-        } else if (!$this->code_model->is_valid($code, $client_id)) {
-            $data['error'] = 'unauthorized_client';
-        
-        } else {
-            // Hooray! Give the lad a token!
-            $this->load->model('access_token_model');
-            $result = $this->access_token_model->create($client_id, $this->session->user);
-            $data['access_token'] = $result->access_token;
-            // Only type supported
-            $data['token_type'] = 'Bearer' ;
-        }
-        
-        
-        $this->output->set_content_type('application/json');
-        $this->output->set_output(json_encode($data));
-    }
-
 }
