66fcec46aab6285bbe4f80cad30b62bb3129cc0b
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2012-07-17 15:42:22 +0200

    set json as content type

--- application/helpers/error_helper.php
+++ application/helpers/error_helper.php
@@ -13,7 +13,8 @@ if ( ! function_exists('show_json_error'))
 	function show_json_error($message, $status_code = 500, $information = array())
 	{
 		set_status_header($status_code);
-
+        header('Content-type: application/json');
+        
 		$error = new stdClass();
 		$error->code = $status_code;
 		$error->message = $message;
