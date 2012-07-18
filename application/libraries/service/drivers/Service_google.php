c4848b615de261966a481c2035867197b8845e62
Lennart Martens <lennerdie@gmail.com>
2012-07-18 17:28:40 +0200

    Service google works

--- application/libraries/service/drivers/Service_google.php
+++ application/libraries/service/drivers/Service_google.php
@@ -81,8 +81,9 @@ class Service_google extends Service_driver {
         $user = $this->api('tokeninfo');
         // valid json response?
         $user = json_decode($user);
+        print_r($user);
         if ( is_null($user) || !isset($user->user_id) ){
-            show_error($error_message . "Could not retrieve userid from Google");
+            show_error($error_message . "Could not retrieve userid from Google. Note: scope must contain 'https://www.googleapis.com/auth/userinfo.profile'");
         }
         
         $auth = new stdClass();
@@ -118,7 +119,7 @@ class Service_google extends Service_driver {
      */
     public function api($endpoint, $params = array(), $method = 'get') {
         $endpoint = rtrim($this->url_base,'/') . '/' . trim($endpoint,'/');
-        $params['oauth_token'] = $this->access_token;
+        $params['access_token'] = $this->access_token;
         
         return $this->oauth->fetch($endpoint, $params, $method);
     }
