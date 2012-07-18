c9bf64104c01b5d46c19baa7f7ad6c0394a70730
Jens Segers <segers.jens@gmail.com>
2012-07-18 13:01:35 +0200

    Ooops

--- application/views/profile/apps.php
+++ application/views/profile/apps.php
@@ -3,15 +3,7 @@
         <h2>Your authorized applications</h2>
             <?php foreach ($results as $result) { ?>
                 <h2><?php echo $result->name ?></h2>
-                <h3>Client Id</h3>
-                <span><?php echo $result->client_id ?></span>
-                <h3>Client Secret</h3>
-                <span><?php echo $result->client_secret ?></span><br />
-                <h3>Callback</h3>
-                <span><?php echo $result->redirect_uri ?></span><br />
-                
                 <a class="btn" href="<?php echo site_url('profile/apps/revoke/'.$result->client_id) ?>">Revoke</a>
-                
                 <hr />
             <?php } ?>
     </div>
