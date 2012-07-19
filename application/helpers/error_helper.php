724e248d6d487cc474602ee5b7b4441d767a1350
Koen De Groote <kdg.private@gmail.com>
2012-07-19 12:05:12 +0200

    Last push in the hunt for tabs.

--- application/helpers/error_helper.php
+++ application/helpers/error_helper.php
@@ -1,30 +1,29 @@
 <?php
 
-/**
-* JSON Error Handler
-*
-* 
-* Display error messages using json format
-*
-* @access	public
-* @return	void
-*/
-if ( ! function_exists('show_json_error'))
-{
-	function show_json_error($message, $status_code = 500, $information = array())
-	{
-		set_status_header($status_code);
-        header('Content-type: application/json');
-        
-		$error = new stdClass();
-		$error->code = $status_code;
-		$error->error = $message;
-		
-		foreach($information as $key=>$val) {
-			$error->$key = $val;
-		}
-		
-		echo json_encode($error);
-		exit();
-	}
-}
+    /**
+     * JSON Error Handler
+     *
+     * 
+     * Display error messages using json format
+     *
+     * @access	public
+     * @return	void
+     */
+    if (!function_exists('show_json_error')) {
+
+        function show_json_error($message, $status_code = 500, $information = array()) {
+            set_status_header($status_code);
+            header('Content-type: application/json');
+
+            $error = new stdClass();
+            $error->code = $status_code;
+            $error->error = $message;
+
+            foreach ($information as $key => $val) {
+                $error->$key = $val;
+            }
+
+            echo json_encode($error);
+            exit();
+        }
+    }
